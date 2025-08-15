51bb5679a08a8695c910cd977bf3836c5b6daba2
Ayato Shiga Callaway <ayato.shigacallaway@bbc.co.uk>
2025-08-15 19:07:09 +0100

    feat: add help command and its message

--- hs-core/Main.hs
+++ hs-core/Main.hs
@@ -7,8 +7,23 @@ main = do
   case args of
     ("toggle" : rest) -> putStrLn $ toggleFormatOnSave (getInputArg rest)
     ("format" : file : _) -> putStrLn $ "Format file: " ++ file -- Replace with your format logic
-    ("version" : _) -> putStrLn "Formatta v0.1.0.0"
-    _ -> putStrLn "Unknown command"
+    ("version" : _) -> putStrLn "Formatta CLI v0.1.0.0"
+    ("help" : _) -> putStrLn helpMessage
+    _ -> putStrLn $ invalidCommandMessage args
+
+invalidCommandMessage :: [String] -> String
+invalidCommandMessage args =
+  unlines $
+    ["Error: Invalid command '" ++ unwords args ++ "'", ""] ++ lines helpMessage
+
+helpMessage :: String
+helpMessage =
+  unlines
+    [ "Formatta CLI - Usage:",
+      "  formatta toggle <true|false>   Toggle format on save.",
+      "  formatta format <file>         Format the given file.",
+      "  formatta version               Show version."
+    ]
 
 getInputArg :: [String] -> String
 getInputArg (x : _) = x
