250bb508b69c27e46a0b7b4bfd216452c3d582cd
Ayato Shiga Callaway <ayato.shigacallaway@bbc.co.uk>
2025-08-15 19:01:41 +0100

    feat: accept command and args

--- hs-core/Main.hs
+++ hs-core/Main.hs
@@ -1,6 +1,15 @@
 import Formatta (toggleFormatOnSave)
+import System.Environment (getArgs)
 
 main :: IO ()
 main = do
-  input <- getLine
-  putStrLn $ toggleFormatOnSave input
+  args <- getArgs
+  case args of
+    ("toggle" : rest) -> putStrLn $ toggleFormatOnSave (getInputArg rest)
+    ("format" : file : _) -> putStrLn $ "Format file: " ++ file -- Replace with your format logic
+    ("version" : _) -> putStrLn "Formatta v0.1.0.0"
+    _ -> putStrLn "Unknown command"
+
+getInputArg :: [String] -> String
+getInputArg (x : _) = x
+getInputArg [] = ""
